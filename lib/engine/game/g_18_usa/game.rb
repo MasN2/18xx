2bb582eb8223275c9f08a6519621f48ac10219f1
MasN2 <28033394+MasN2@users.noreply.github.com>
2025-09-29 00:56:08 -0400

    Update game.rb
    Private company setup rules

--- lib/engine/game/g_18_usa/game.rb
+++ lib/engine/game/g_18_usa/game.rb
@@ -280,8 +280,7 @@ module Engine
 
         def randomize_privates
           always_in = %w[P1 P2 P3 P4]
-          always_in << 'P10' if @players.size >= 5
-          num_kept = { 30 => 1, 40 => 2, 60 => 3, 80 => 2, 90 => 2, 120 => 1 }
+          num_kept = { 40 => 2, 60 => 3, 80 => 3, 120 => 1 }
 
           to_remove = @companies.reject { |c| always_in.include?(c.id) }.group_by(&:value).flat_map do |val, companies|
             companies.sort_by { rand }.take(companies.size - num_kept[val])
