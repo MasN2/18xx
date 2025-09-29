b38c549fb3757f6eebf65ba8543c210663cbfa5c
MasN2 <28033394+MasN2@users.noreply.github.com>
2025-09-29 01:10:07 -0400

    Update operating.rb
    12.9. Voluntary Defaulting

--- lib/engine/game/g_18_usa/round/operating.rb
+++ lib/engine/game/g_18_usa/round/operating.rb
@@ -24,11 +24,32 @@ module Engine
           end
 
           def pay_interest!(entity)
-            # 1817's pay_interest! does a 'return unless step_passed?(Engine::Step::BuyTrain)' which unintentionally
-            #   passes for the 18USA BuyPullmanStep - here we check that 18USA BuyTrain is passed before continuing
-            return unless step_passed?(G18USA::Step::BuyTrain)
+            @cash_crisis_due_to_interest = nil
+            return if @paid_loans[entity]
+            return unless step_passed?(Engine::Step::BuyTrain)
 
-            super
+            @paid_loans[entity] = true
+            return if @game.interest_owed(entity).zero?
+
+            bank = @game.bank
+            owed = interest_owed(entity)
+            if owed.positive?
+              log_interest_payment(entity, owed)
+              entity.spend(owed, bank)
+            end
+            return unless entity.cash.negative?
+
+            owed_fmt = @game.format_currency(-entity.cash)
+
+            owner = entity.owner
+            @game.liquidate!(entity)
+            transferred = ''
+
+            @log << "#{entity.name} is #{owed_fmt} short on interest and goes into liquidation#{transferred}"
+
+            owner.spend(-entity.cash, entity, check_cash: false)
+            @cash_crisis_due_to_interest = entity
+            @log << "#{owner.name} pays #{owed_fmt} shortfall on interest"
           end
         end
       end
